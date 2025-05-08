2a80947e05463dc3cf64a16f4af489a15d54cb99
RandazzoB <randazzoesmetbruno@gmail.com>
2025-05-08 12:29:10 -0300

    Revert "Práctica ejercicios Bucless"
    This reverts commit 7d8ebfb51035fb68b0f94205ab9c3de0858d1485.

--- C6 Bucles/ejercicios/03.js
+++ C6 Bucles/ejercicios/03.js
@@ -3,12 +3,6 @@ function obtenerMayor(x, y) {
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
   // Tu código:
-  if(x === y) return x || y;
-  else if (x > y) return x;
-   else return y;
 }
 
-console.log(obtenerMayor(7,7));
-console.log(obtenerMayor(9,7));
-console.log(obtenerMayor(7,10));
 module.exports = obtenerMayor;
--- C6 Bucles/ejercicios/05.js
+++ C6 Bucles/ejercicios/05.js
@@ -2,11 +2,6 @@ function esVerdadero(valor) {
   // Si "valor" es verdadero, retorna "Soy verdadero".
   // Caso contrario, retorna "Soy falso".
   // Tu código:
-  if(valor === true) return 'Soy verdadero';
-  else return 'Soy falso'
 }
 
-console.log(esverdadero(true));
-console.log(esverdadero(false));
-console.log(esverdadero(7));
 module.exports = esVerdadero;
